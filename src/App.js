1da3c1b3d5c93915157d17ffb36f102ce87041f7
Khalil <khalilfoulks@gmail.com>
2020-06-19 11:23:47 -0400

    added dependencies and file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import HomePage from './components/HomePage'
+import { Switch, Route, Link } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
+      <div className='nav-links'>
+          <Link to='/'>Home</Link>
+          <Link to='/pizza'>Make Pizza</Link>
+      </div>
     </>
   );
 };
---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <section className="home-page">
+      <header>
+        <img
+          className="main-img"
+          src='/Assets/Pizza.jpg'
+          alt="Pizza"
+        />
+      </header>
+    </section>
+  );
+}
